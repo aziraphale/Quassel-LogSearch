cd23795054e2dd400523e4f3383ec5f44bbe20c5
m4yer <m4yer@minad.de>
2009-08-04 16:07:58 +0200

    update about-page

--- about.php
+++ about.php
@@ -1,16 +1,17 @@
 <center>
+<b style="font-size:13pt;">Quassel Backlog Search</b><br>Version: 0.1
+<br><br><br><br>
 A webbased <b>Quassel-Search-Engine</b> for <a target="_blank" href="http://quassel-irc.org/">Quassel IRC</a> (with PostgreSQL backend).
-<br><br>
-developed 2009 by <a href="http://m4yer.minad.de/?page=5" target="_blank">m4yer</a>.
 <br>
-<b>Quassel-Search-Engine</b> is licensed under a <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" target="_blank">Creative Commons Licence</a>.
-<br><br>
+<b>Quassel Backlog Search</b> is licensed under a <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" target="_blank">Creative Commons Licence</a>.
+<br>developed 2009 by <a href="http://m4yer.minad.de/?page=5" target="_blank">m4yer</a>.
+<br><br><br><br>
 <a target="_blank" href="http://quassel-irc.org/">Quassel IRC</a> is a modern, cross-platform, distributed IRC client.<br>
 ©2005-2009 by the Quassel Project.<br>
 <a target="_blank" href="http://quassel-irc.org/">http://quassel-irc.org</a>.
-<br><br>
+<br><br><br><br>
 Most icons are &copy; by the <a href="http://www.oxygen-icons.org/" target="_blank">Oxygen Team</a>, used under <a href="http://www.gnu.org/licenses/lgpl.html" target="_blank">LGPL</a> and adopted from <a target="_blank" href="http://quassel-irc.org/">Quassel IRC</a>.
-<br><br><br><b>Special thanks go to:</b><br>
+<br><br><br><br><b>Special thanks go to:</b><br>
 <b>brot</b> for bugfixing, testing, helping, finding features and bugs, building the environment, advertising, motivating and a lot more.<br>
 <b>the Quassel Community</b> for feedback, finding bugs and using.<br>
 <b><a target="_blank" href="http://quassel-irc.org/">Quassel IRC</a></b> for developing such a great IRC-Client.<br><br>
