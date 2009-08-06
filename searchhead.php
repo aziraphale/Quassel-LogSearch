4fcd38078e284317c774e2c0b484c1c225f06992
m4yer <m4yer@minad.de>
2009-08-06 15:28:34 +0200

    time-search-gui added to advanced search

--- searchhead.php
+++ searchhead.php
@@ -4,8 +4,8 @@
 <img style="float:left;margin:10px;margin-bottom:-20px; margin-right:60px;" alt="" src="style/quassel.png">
 
 <div style="padding: 20px; float:left;">
-<span style="font-size: 20pt;color:white;">Quassel Backlog Search</span><br><br>
 <form onsubmit="javascript:such(); return false" action="#">
+<span style="font-size: 20pt;color:white;">Quassel Backlog Search</span><br><br>
 <input value="" id="input" size="60" type="text"">&nbsp;<input type="submit" value="Start searching!" class="button">
 <br><img src="style/channel.png" style="z-index:1;position:relative;margin:2px;float:left;" alt="Buffer" title="Buffer to search">
 <script type="text/javascript">
@@ -37,11 +37,13 @@ pg_close($dbconn);
 ?>
 </select><img src="style/lines.png" title="Number of results" alt="Lines" style="margin:2px; margin-left:20px;float:left;"><input title="Number of results" value="100" id="number" size="3" type="text" maxlength="4"  autocomplete="off">
 <span onclick="document.getElementById('advanced').style.display='block';" style="font-size:7pt;margin-left:20px;">[ Advanced search ]</span>
-<br>
-</form>
-</div>
-<div id="advanced" style="padding: 10px;float:left;display:none;">
-<span onclick="document.getElementById('advanced').style.display='none';">Advanced search</span>
-</div>
+<br> </div>
+<div id="advanced" style="margin: 7px; padding: 7px; float:left;display:none; font-size:7pt; border-left:1px solid white">
+    <span onclick="document.getElementById('advanced').style.display='none';" style="margin-left:-3px;">[ Advanced search ]</span><br>
+    Search in a period:<br>
+    <input name="time_start" size="20;" value="Starttime" onBlur="if(this.value=='') this.value='Starttime';" onFocus="if(this.value=='Starttime') this.value='';">
+    <input name="time_ende" size="20;" value="Endtime" onBlur="if(this.value=='') this.value='Endtime';" onFocus="if(this.value=='Endtime') this.value='';">
+<br><br>
+</div></form>
 </div><center>
 <div style="clear:both;width:100%;"><div id="scontent" style="overflow:auto;padding: 20px; text-align:left; vertical-align:top;"><font style="font-size:8pt;"><center>Waiting for search ...</center></font></div></div>
