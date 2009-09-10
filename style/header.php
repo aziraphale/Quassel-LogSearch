efd7a989a002dd7231e5341da23ed8ca298ac730
m4yer <m4yer@minad.de>
2009-09-10 14:52:20 +0200

    make header english :) everything should be in one language now

--- style/header.php
+++ style/header.php
@@ -2,11 +2,11 @@
 <html>
 <head>
           <meta name="robots" content="index,follow">
-          <meta http-equiv="Content-Language" content="de">
+          <meta http-equiv="Content-Language" content="en">
           <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
           <meta http-equiv="Content-Style-Type" content="text/css">
 <link rel="shortcut icon" href="style/bluemarine_favicon_0.png" type="image/x-icon" />
-          <title>Quassel-Suche</title>
+          <title>Quassel Backlog Search</title>
      <base target="_self">
     <link rel="stylesheet" href="style/style.css" media="screen,projection" type="text/css">
      <script type="text/javascript" src="style/prototype.js"></script>
