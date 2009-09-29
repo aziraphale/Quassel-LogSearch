843a04acb4352e73fb21a92fcfc0f2feab1c4ad9
Martin Mayer <m4yer@minad.de>
2009-09-29 22:46:11 +0200

    basic debug-function yet not useable, but under construction

--- classes/debug.class.php
+++ classes/debug.class.php
@@ -1,4 +1,27 @@
 <?php
-//debug class
+//debug class - optional
+
+class debug {
+    
+    function debug($type,$string) {
+        // nimmt fehler auf, leitet richtig weiter und gibt aus
+        switch $type {
+            case 1: // normale fehler
+            $error = '';
+            break;
+            case 2: // oop
+            $error = '';
+            break;
+            case 3: // debugdurchleitung
+            $error = '<br>::DEBUG:: '.$string.' ::DEBUG::<br>';
+            break;
+            default:
+            die('Fatal Error - Unknown Errortype!')
+            } 
+        
+        return $error;
+        }
+    
+    }
 
 ?>
