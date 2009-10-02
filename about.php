005f69574156d956c40891cc5f8f6553a32b901e
Martin Mayer <m4yer@minad.de>
2009-10-02 21:59:18 +0200

    small about-page update

--- about.php
+++ about.php
@@ -24,7 +24,7 @@ $array[] = $file;
 A webbased <b>Quassel-Search-Engine</b> for <a target="_blank" href="http://quassel-irc.org/">Quassel IRC</a>.
 <br>
 <b>Quassel Backlog Search</b> is licensed under a <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" target="_blank">Creative Commons Licence</a>.
-<br>developed 2009 by <a href="http://m4yer.minad.de/?page=5" target="_blank">m4yer</a>.
+<br>developed and copyright 2009 by <a href="http://m4yer.minad.de/?page=5" target="_blank">m4yer</a>.
 <br><br><br><br>
 <a target="_blank" href="http://quassel-irc.org/">Quassel IRC</a> is a modern, cross-platform, distributed IRC client.<br>
 ©2005-2009 by the Quassel Project.<br>
@@ -33,7 +33,7 @@ A webbased <b>Quassel-Search-Engine</b> for <a target="_blank" href="http://quas
 Most icons are &copy; by the <a href="http://www.oxygen-icons.org/" target="_blank">Oxygen Team</a>, used under <a href="http://www.gnu.org/licenses/lgpl.html" target="_blank">LGPL</a> and adopted from <a target="_blank" href="http://quassel-irc.org/">Quassel IRC</a>.
 <br><br><br><br><b>Special thanks go to:</b><br>
 <b>brot</b> for bugfixing, testing, helping, finding features and bugs, building the environment, advertising, motivating and a lot more.<br>
-<b>the Quassel IRC Community</b> for feedback, finding bugs and using.<br>
-<b><a target="_blank" href="http://quassel-irc.org/">the Quassel IRC Team</a></b> for developing such a great IRC-Client.<br><br>
+<b>The Quassel IRC Community</b> for feedback, finding bugs and using.<br>
+<b><a target="_blank" href="http://quassel-irc.org/">The Quassel IRC Team</a></b> for developing such a great IRC-Client.<br><br>
 
 </center>
