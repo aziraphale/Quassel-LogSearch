de27e9ff581bfc025ed724927e35a7646f6d3983
m4yer <m4yer@minad.de>
2009-10-04 18:02:55 +0200

    fix logout and add debuger to it

--- logout.php
+++ logout.php
@@ -4,6 +4,8 @@
 //      developed 2009 by m4yer <m4yer@minad.de> under a Creative Commons Licence by-nc-sa 3.0
 //
 
+require_once('debuger.php');
+session_start();
 session_destroy();
 header('Location: /');
 ?>
