8dfb4d310e15c6aa6e1f67d22451c5837dcedf4a
Martin Mayer <m4yer@minad.de>
2009-10-06 11:06:56 +0200

    rearange index.php

--- index.php
+++ index.php
@@ -10,39 +10,43 @@ if (session_id() == ""){
     @session_start();
 }
 
+// php not to old?
+if (version_compare(PHP_VERSION, '5.1.0') !== 1) {  // 5.1 ist notwenig wegen OOP
+     die('<b>Your php-Version is too old - please update at least to 5.1!</b>');
+}
 
-$mobile = FALSE;
-// mobile umleitung
-if(preg_match('/(IEMobile|Windows CE|NetFront|PlayStation|PLAYSTATION|like Mac OS X|MIDP|UP\.Browser|Symbian|Nintendo|Pocket|Mobile|Portable|Smartphone|SDA|PDA|Handheld|WAP|Palm|Avantgo|cHTML|BlackBerry|Opera\.Mini|Nokia)/', $_SERVER['HTTP_USER_AGENT'])){
-    $mobile = TRUE;
-    $_SESSION['mobile'] = TRUE;
-    }
-
-// make sure both vars are set.
-if((isset($_SESSION['mobile'])) AND $_SESSION['mobile'] == TRUE){
-    $mobile = TRUE;
-    }else{
-        $mobile = FALSE
+// mobile-redirection
+    $mobile = FALSE;
+    // mobile umleitung
+    if(preg_match('/(IEMobile|Windows CE|NetFront|PlayStation|PLAYSTATION|like Mac OS X|MIDP|UP\.Browser|Symbian|Nintendo|Pocket|Mobile|Portable|Smartphone|SDA|PDA|Handheld|WAP|Palm|Avantgo|cHTML|BlackBerry|Opera\.Mini|Nokia)/', $_SERVER['HTTP_USER_AGENT'])){
+        $mobile = TRUE;
+        $_SESSION['mobile'] = TRUE;
+        }
+    
+    // make sure both vars are set.
+    if((isset($_SESSION['mobile'])) AND $_SESSION['mobile'] == TRUE){
+        $mobile = TRUE;
+        }else{
+            $mobile = FALSE
+            }
+    
+    // want normal?
+    if((isset($_REQUEST['force_standard'])) AND $_REQUEST['force_standard'] == TRUE){
+        $mobile = FALSE;
+        $_SESSION['mobile'] = FALSE;
         }
 
-// want normal?
-if((isset($_REQUEST['force_standard'])) AND $_REQUEST['force_standard'] == TRUE){
-    $mobile = FALSE;
-    $_SESSION['mobile'] = FALSE;
-    }
 
+//
+//  build design
+//
 
-//mobile header
+// mobile header
 if($mobile){
     include_once('style/header.mobile.php');
     }else{
         include_once('style/header.php');
         }
-
-// php not to old?
-if (version_compare(PHP_VERSION, '5.1.0') !== 1) {  // 5.1 ist notwenig wegen OOP
-     die('<b>Your php-Version is too old - please update at least to 5.1!</b>');
-}
 
 
 // active session or login-try or none?
@@ -55,7 +59,7 @@ if(isset($_SESSION['pwdn']) OR !empty($_SESSION['pwdn'])){
                 include_once('loginform.php');
                 }
 
-//mobile footer
+// mobile footer
 if($mobile){
     include_once('style/footer.mobile.php');
     }else{
