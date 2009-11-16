707f6c9387942ded6d25a2217c082908798d9a28
m4yer <m4yer@minad.de>
2009-11-16 20:42:16 +0100

    reduce debug output and add time to debuglogs

--- debuger.php
+++ debuger.php
@@ -17,7 +17,7 @@ require('classes/debug.class.php');
 function exception_error_handler($errno, $errstr, $errfile, $errline) {
         //log in hourly changing logfiles
         $fp = fopen ('debug-'.date('d-m-Y-H').'.log','a+');
-        fwrite ($fp,$errstr.' - '.array_pop(explode('/',$errfile)).':'.$errline.' ('.$errno.')'."\n");
+        fwrite ($fp,date('d-m-Y-H:i:s').' - '.$errstr.' - '.array_pop(explode('/',$errfile)).':'.$errline.' ('.$errno.')'."\n");
         fclose ($fp);
         $array = explode('/',$errfile);
     //print error but dont die (only if fatal)
@@ -30,7 +30,7 @@ if($debug == 'debugfull'){
     set_error_handler("exception_error_handler", -1);    //set fullhandler
     $debuger = new debughandler();  //set for further debuging and backtracing debug-objekt
     }elseif($debug == 'debug'){
-        set_error_handler("exception_error_handler", E_ALL^E_STRICT);    //set handler
+        set_error_handler("exception_error_handler", E_ALL);    //set handler
         $debuger = new debughandler();  //set for further debuging and backtracing debug-objekt
         }else{  //set no handler => no errors.
             $debuger = new no_debughandler();   //set empty object to avoid debuging
