b177caa6524f383cb3feb6c45154d9c4834d9677
m4yer <m4yer@minad.de>
2010-02-28 23:35:51 +0100

    exclude parsing of config from config-file.

---
+++ parseconf.php
@@ -0,0 +1,15 @@
+<?php
+//
+//      Quassel Backlog Search
+//      developed 2009-2010 by m4yer <m4yer@minad.de> under a Creative Commons Licence by-nc-sa 3.0
+//
+
+
+// parse timezone
+    $dateTimeZone= new DateTimeZone($timezone);
+    $dateTime = new DateTime("now", $dateTimeZone);
+    ini_set('date.timezone', $timezone);
+    $timezone = $dateTimeZone->getOffset($dateTime)/36;
+
+ 
+?>
