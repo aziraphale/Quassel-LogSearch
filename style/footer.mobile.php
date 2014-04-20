c82012c00a7c2dabde59f05aa1d7c5a3c4dd8585
Andrew Gillard <andrew@lorddeath.net>
2014-04-20 19:11:18 +0100

    Deleted all existing code ready to start afresh.

--- style/footer.mobile.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if((isset($loggedin)) AND $loggedin == TRUE){
-        ?>
-<a href="logout.php"><?php echo _('logout')?></a>&nbsp;|&nbsp;<a href="index.php?force_standard=TRUE"><?php echo _('standard')?></a>&nbsp;|&nbsp;<a href="javascript:Request('scontent','about.php');"><?php echo _('about')?></a>
-<?php
-    }
-?></center></div><br><br></body></html>
--- style/footer.php
+++
@@ -1,1 +0,0 @@
-<br></body></html>
