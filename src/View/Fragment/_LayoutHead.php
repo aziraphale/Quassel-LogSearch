0675c7e6fb2e012e2249480d6bd6ddb59181e2f5
Andrew Gillard <andrew@lorddeath.net>
2014-08-03 17:46:12 +0100

    Pulled in a load of new JS libs via Bower.

--- src/View/Fragment/_LayoutHead.php
+++ src/View/Fragment/_LayoutHead.php
@@ -5,25 +5,44 @@
     <base href="<?=$this->escape($this->baseUrl)?>">
     <title>Quassel Log Search</title>
 
-    <!-- Bootstrap -->
-    <link href="public/vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="public/vendor/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <script src="public/vendor/jquery/jquery.min.js"></script>
+    <!-- ##### jQuery ##### -->
+    <script src="public/vendor/jquery/dist/jquery.min.js"></script>
     <!--<script src="public/vendor/jquery/jquery-migrate.min.js"></script>-->
+
+    <!-- ##### jQuery UI ##### -->
     <link rel="stylesheet" href="public/vendor/jquery-ui/themes/base/minified/jquery-ui.min.css">
     <script src="public/vendor/jquery-ui/ui/minified/jquery-ui.min.js"></script>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- ##### Bootstrap ##### -->
+    <link href="public/vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="public/vendor/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
     <script src="public/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
 
+    <!-- ##### Autolinker.js ##### -->
     <script src="public/vendor/Autolinker.js/dist/Autolinker.min.js"></script>
 
+    <!-- ##### Bootbox ##### -->
+    <script src="public/vendor/bootbox/bootbox.js"></script>
+
+    <!-- ##### Bootstrap Switch ##### -->
+    <link href="public/vendor/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.min.css" rel="stylesheet">
+    <script src="public/vendor/bootstrap-switch/dist/js/bootstrap-switch.min.js"></script>
+
+    <!-- ##### History.js ##### -->
+    <script src="public/vendor/history.js/scripts/compressed/history.js"></script>
+    <script src="public/vendor/history.js/scripts/compressed/history.adapter.jquery.js"></script>
+
+    <!-- ##### Humane.js ##### -->
+    <link href="public/vendor/humane-js/themes/jackedup.css" rel="stylesheet">
+    <script src="public/vendor/humane-js/humane.min.js"></script>
+
+    <!-- ##### Quassel LogSearch -->
     <link href="public/style/screen.css" media="screen, projection" rel="stylesheet">
     <link href="public/style/print.css" media="print" rel="stylesheet">
     <!--[if IE]>
